20c1f7a5b4967a2acd3d8742511fdb860f7b35ed
uwesterr <uwe.sterr@st2c.degit config --global user.name napon123>
2020-06-30 19:59:47 +0200

    added images changed to NavBar design

--- app.R
+++ app.R
@@ -1,12 +1,21 @@
 # can be tested with Free statistical calculators https://www.medcalc.org/calc/diagnostic_test.php
-# theory is given at https://de.wikipedia.org/wiki/Beurteilung_eines_binären_Klassifikators#Positiver_und_negativer_Vorhersagewert
+# theory is given at https://de.wikipedia.org/wiki/Beurteilung_eines_binären_Klassifikators#positivr_und_negativr_Vorhersagewert
+if (!("caret" %in% rownames(installed.packages()))) install.packages("caret")
+
 library(shiny)
 library(tidyverse)
 library(plotly)
+library(shinyWidgets)
+library(caret)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- function(request) {
     
+    
+    # widgets website https://shiny.rstudio.com/gallery/widget-gallery.html
+    navbarPage("Aussagekraft von Medizinischen Tests",  position = c("static-top"), theme = "bootstrap.css", 
+               
+               tabPanel("Einstellung und Ausgabe",
     # Application title
     tags$head(tags$style('h3 {color:blue;}')),
     tags$head(tags$style('h2 {color:blue;}')),
@@ -15,69 +24,184 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         
-        sidebarPanel(
-            h3("Test Parameter"),
-            numericInput("sensitivity", label = "Sensivität [%]", value = 90, min=0, max=100, step=.1), # numbers are from https://www.cegat.de/diagnostik/corona-diagnostik/
-            numericInput("specificity", label = "Spezifität [%]", value = 90, min=0, max=100, step=.1),
-            numericInput("prevalence", label = "Basisrate [%]", value = 10, min=0, max=100, step=.1),
+        
+        sidebarPanel( width = 5,
+            setBackgroundColor("#ECF0F5"
+                               # color = c("#F7FBFF", "#2171B5"),
+                               # gradient = "radial",
+                               # direction = c("top", "left")
+            ),
+            h3("Testparameter"),
+            numericInput("sensitivity", label = h4("Sensivität [%]"), value = 94.4, min=0, max=100, step=.1), # numbers are from https://www.cegat.de/diagnostik/corona-diagnostik/
+            numericInput("specificity", label = h4("Spezifität [%]"), value = 99.6, min=0, max=100, step=.1),
+            numericInput("prevalence", label = h4("Basisrate [%]"), value = 0.85, min=0, max=100, step=.1),
             wellPanel(
-                h3("Ausgewählte Szenarien"),
+                h3("Vordefinierte Szenarien"),
                 helpText("Wählen Sie ein Szenario das Sie interessiert"),
                 
                 actionButton("Antikoerper", label = "Antikörper"),
                 actionButton("Mammographie", label = "Mammographie"),
-                actionButton("Schwangerschaft", label = "Schwangerschaft"),
-                actionButton("Anleitung", label = "Anleitung")),
+                actionButton("PSA", label = "PSA")),
+               # actionButton("Anleitung", label = "Anleitung")),
+            wellPanel(
+            h3("Wahrheitsmatrix für 100.000 Getestete "),
+            plotOutput("confusionMat")),
+            helpText("In der Wahrheitsmatrix geben die Zahlen in den grün hinterlegten Felder korrekte Testergebnisse, 
+                     in den rot hinterlegten Feldern inkorrekte Anzahl von Testfällen wieder"),
+            tags$hr(),
+            helpText("Je mehr Fälle inkorrekt postiv getestet werden, desto geringer ist die Relevanz da die Testperson nicht unterscheiden kann ob sie korrekt oder inkorrekt positiv getestet wurde."),
         ),
         
         
         # Show a plot of the generated distribution
-        mainPanel(
+        mainPanel( width = 7,
             wellPanel(
                 
                 wellPanel(
-                    h2("Was bedeutet  Testergebnis für den Getesteten?"),
-                    helpText("Was bedeutet ein positives bzw. \n negatives Testergebnis für den Getesteten?"),
-                    HTML("<strong>Relevanz:</strong> Wahrscheinlichkeit das Getestete positiv ist bei positiven Testergebnis \n"),
+                    h2("Was bedeutet ein Testergebnis für den Getesteten?"),
+                    helpText("Was bedeutet ein positives bzw. negatives Testergebnis für den Getesteten?"),
+                    HTML("<strong>Relevanz:</strong> Wahrscheinlichkeit, dass der Getestete bei positivem Testergebnis positiv ist"),
                     p(),
-                    HTML("<strong>Trennfähigkeit:</strong> Wahrscheinlichkeit das Getestete negativ ist bei negativem Testergebnis"),
+                    HTML("<strong>Trennfähigkeit:</strong> Wahrscheinlichkeit, dass der Getestete bei negativem Testergebnis negativ ist "),
                     
                     
                     fluidRow(
                         column(6,
-                               h5("Relevanz [%]"),
+                               h3("Relevanz [%]"),
                                tags$head(tags$style('h5 {color:blue;}')),
                                verbatimTextOutput("PvPlus", placeholder = TRUE)),
                         column(6,
                                
-                               h5("Trennfähigkeit [%]"),
+                               h3("Trennfähigkeit [%]"),
                                verbatimTextOutput("pvMinus", placeholder = TRUE)
                         ))),
+                h3("Einfluß der Basisrate"),
+                helpText("Den Einfluss der Basisrate auf
+
+Relevanz und
+Trennfähigkeit
+wird im nachfolgenden Graph verdeutlicht. Die grüne Linie zeigt den Wert der eingestellten Basisrate dar, Werte für Relevanz und Trennfähigkeit für andere Basisratenwerte können aus dem Graph abgelesen werden."),
+                tags$hr(),              
                 plotlyOutput("distPlot")),
+ 
             wellPanel(
                 uiOutput("md_file"))
             
         )
     )
-)
+),
+
+
+           tabPanel("Anleitung",
+                    
+                    # Show a plot of the generated distribution
+                    mainPanel(
+                        withMathJax(includeMarkdown("Anleitung.md"))
+                    )
+           ))        
+           
+
+}
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+
+  
+    output$confusionMat <-  renderPlot({
+       # browser()
+        total <- 1e5
+        
+        positiv <- (total * input$prevalence/100) %>% round(digits = 0)
+        negativ <-( total -positiv) %>% round(digits = 0)
+        TP <- (positiv * input$sensitivity/100) %>% round(digits = 0)
+        TN  <- (negativ * input$specificity/100) %>% round(digits = 0)
+        FN <-  positiv - TP
+        FP <- negativ - TN
+        
+        
+        predictions <- c(
+            rep("positiv",TP),
+            rep("negativ", FN),
+            rep("negativ", TN),
+            rep("positiv", FP)) %>% factor(levels = c("positiv", "negativ"))
+        
+        
+        groundtruth <- c(
+            rep("positiv", positiv),
+            rep("negativ", negativ)) %>% factor(levels = c("positiv", "negativ"))
+        xtab <- table(predictions, groundtruth)
+        
+        cmtrx <- confusionMatrix(xtab, positiv = "positiv")
+        total <- sum(cmtrx$table)
+        res <- as.numeric(cmtrx$table)
+        # Generate color gradients. Palettes come from RColorBrewer.
+        greenPalette <- c("#F7FCF5","#E5F5E0","#C7E9C0","#A1D99B","#74C476","#41AB5D","#238B45","#006D2C","#00441B")
+        redPalette <- c("#FFF5F0","#FEE0D2","#FCBBA1","#FC9272","#FB6A4A","#EF3B2C","#CB181D","#A50F15","#67000D")
+        getColor <- function (greenOrRed = "green", amount = 0) {
+            if (amount == 0)
+                return("#A1D99B")
+            palette <- greenPalette
+            if (greenOrRed == "red")
+                palette <- redPalette
+            colorRampPalette(palette)(100)[10 + ceiling(90 * amount / total)]
+        }
+        # set the basic layout
+        layout(matrix(c(1,1,1)))
+        par(mar=c(3,3,3,3))
+        plot(c(100, 345), c(300, 450), type = "n", xlab="", ylab="", xaxt='n', yaxt='n')
+       # title('Wahrheitsmatrix für 100.000 Getestete \n', cex.main=3.5)
+        # create the matrix
+        classes = colnames(cmtrx$table)
+        
+      
+        rect(135, 370, 345, 430, col= "#b3b3ff", density = 80) # positiv test
+        rect(150, 441, 240, 300, col= "#b3b3ff", density = 80)  # positiv wahrheit
+        rect(250, 441, 340, 300, col= "#e6b3ff", density = 80)  # negativ wahrheit
+        text(195, 435, classes[1], cex=2.7)
+       
+        rect(150, 430, 240, 370, col= "#A1D99B")
+        rect(250, 430, 340, 370, col= "#FC9272")
+        text(295, 435, classes[2], cex=2.7)
+        text(125, 370, 'Testergebnis', cex=2.7, srt=90, font=2)
+        text(245, 450, 'Wahrheit', cex=2.7, font=2)
+        rect(135, 305, 345, 365, col= "#e6b3ff", density = 80) # negativ test
+        rect(150, 305, 240, 365, col= "#FC9272")
+        rect(250, 305, 340, 365, col= "#A1D99B")
+        text(140, 400, classes[1], cex=2.7, srt=90)
+        text(140, 335, classes[2], cex=2.7, srt=90)
+        # add in the cmtrx results
+        text(195, 400, res[1], cex=2.6, font=2, col='blue')
+        text(195, 335, res[2], cex=2.6, font=2, col='blue')
+        text(295, 400, res[3], cex=2.6, font=2, col='blue')
+        text(295, 335, res[4], cex=2.6, font=2, col='blue')
+        # add in the specifics
+#       plot(c(100, 0), c(100, 0), type = "n", xlab="", ylab="", main = "DETAILS", xaxt='n', yaxt='n')
+#       text(10, 85, names(cmtrx$byClass[1]), cex=1.7, font=2)
+#       text(10, 70, round(as.numeric(cmtrx$byClass[1]), 3), cex=1.7)
+#       text(30, 85, names(cmtrx$byClass[2]), cex=1.7, font=2)
+#       text(30, 70, round(as.numeric(cmtrx$byClass[2]), 3), cex=1.7)
+#       text(50, 85, names(cmtrx$byClass[5]), cex=1.7, font=2)
+#       text(50, 70, round(as.numeric(cmtrx$byClass[5]), 3), cex=1.7)
+#       text(70, 85, names(cmtrx$byClass[6]), cex=1.7, font=2)
+#       text(70, 70, round(as.numeric(cmtrx$byClass[6]), 3), cex=1.7)
+#       text(90, 85, names(cmtrx$byClass[7]), cex=1.7, font=2)
+#       text(90, 70, round(as.numeric(cmtrx$byClass[7]), 3), cex=1.7)
+#       # add in the accuracy information
+#       text(30, 35, names(cmtrx$overall[1]), cex=1.7, font=2)
+#       text(30, 20, round(as.numeric(cmtrx$overall[1]), 3), cex=1.4)
+#       text(70, 35, names(cmtrx$overall[2]), cex=1.7, font=2)
+#       text(70, 20, round(as.numeric(cmtrx$overall[2]), 3), cex=1.4)
+    })
+    
+
     output$md_file <- renderUI({
-        file <-  "Anleitung.md"
+        file <-  "Antikoerper.md"
         withMathJax(
             includeMarkdown(file))
     })
     
     
-    observeEvent(input$Anleitung, {
-        
-        output$md_file <- renderUI({
-            file <-  "Anleitung.md"
-            withMathJax(
-                includeMarkdown(file))
-        })
-    })
+
     
     observeEvent(input$Antikoerper, {
         updateNumericInput(session, inputId = "sensitivity", value = 94.4)  # numbers are from https://www.cegat.de/diagnostik/corona-diagnostik/
@@ -104,13 +228,12 @@ server <- function(input, output, session) {
         })
     })
     
-    observeEvent(input$Schwangerschaft, {
-        #https://www.laekh.de/images/Hessisches_Aerzteblatt/2016/12_2016/CME_12_2016_Was_Aerzte_wissen_muessen.pdf
-        updateNumericInput(session, inputId = "sensitivity", value = 99.2)  # numbers are from https://www.cegat.de/diagnostik/corona-diagnostik/
-        updateNumericInput(session, inputId = "specificity", value = 99.91)
-        updateNumericInput(session, inputId = "prevalence", value = 1/86) # https://www.ndr.de/nachrichten/info/coronaskript210.pdf seite 6
+    observeEvent(input$PSA, {
+        updateNumericInput(session, inputId = "sensitivity", value = 91)  
+        updateNumericInput(session, inputId = "specificity", value = 91)
+        updateNumericInput(session, inputId = "prevalence", value = 0.1) # https://www.prostata.de/prostatakrebs/was-ist-pca/haeufigkeit-des-prostatakarzinoms
         output$md_file <- renderUI({
-            file <-  "Mammographie.md"
+            file <-  "PSA.md"
             withMathJax(
                 includeMarkdown(file))
         })
@@ -130,19 +253,29 @@ server <- function(input, output, session) {
                              pvPlus = (prevalence*sensitivity)/((prevalence*sensitivity)+(1-prevalence)*(1-specificity)),
                              pvMinus = ((1-prevalence)*specificity)/(((1-prevalence)*specificity)+(prevalence*(1-sensitivity)))
         ) 
-        output$PvPlus <- renderText({ data_point$pvPlus %>% round(digits = 6) *100})
-        output$pvMinus <- renderText({ data_point$pvMinus %>% round(digits = 6) *100})
+        output$PvPlus <- renderText({ data_point$Relevanz %>% round(digits = 6) *100})
+        output$pvMinus <- renderText({ data_point$Trennfähigkeit %>% round(digits = 6) *100})
+       # browser()
+        colnames(data)[colnames(data) == "prevalence"] <- "Basisrate"
+        colnames(data)[colnames(data) == "pvPlus"] <- "Relevanz"
+        colnames(data)[colnames(data) == "pvMinus"] <- "Trennfähigkeit"
         
+        colnames(data_point)[colnames(data_point) == "prevalence"] <- "Basisrate"
+        colnames(data_point)[colnames(data_point) == "pvPlus"] <- "Relevanz"
+        colnames(data_point)[colnames(data_point) == "pvMinus"] <- "Trennfähigkeit"
+       # browser()
         # draw the histogram with the specified number of bins
-        p <-  ggplot(data, aes(prevalence, pvPlus, color = "Relevanz")) + geom_line() + geom_line(aes(y = pvMinus, color = "Trennfähigkeit")) +
-            geom_point(data = data_point,aes(x = prevalence, y = pvMinus, color = "Trennfähigkeit")) + geom_point(data = data_point,aes(x = prevalence, y = pvPlus, color = "Relevanz")) +
-            scale_color_manual(values = c('Relevanz' = "red",'Trennfähigkeit' = "blue")) + 
-            labs( y = "Wahrscheinlichkeit", x = "Basisrate", title = "Grüne Linie ist bei der gewählten Basisrate") + 
-            labs(color='Vorhersagewert') + xlim(0,1) +geom_vline(xintercept = data_point$prevalence, color = "green") #+
+        p <-  ggplot(data, aes(x= Basisrate, Relevanz, color = "Relevanz")) + geom_line() + geom_line(aes(y = Trennfähigkeit, color = "Trennfähigkeit")) +
+            geom_point(data = data_point,aes(x = Basisrate, y = Trennfähigkeit, color = "Trennfähigkeit")) + geom_point(data = data_point,aes(x = Basisrate, y = Relevanz, color = "Relevanz")) +
+            labs( y = "Wahrscheinlichkeit", x = "Basisrate") + 
+            labs(color='Vorhersagewert') + xlim(0,1) +geom_vline(aes(xintercept = data_point$Basisrate, color = "Eingestellte Basisrate")) +
+            scale_color_manual(values = c('Relevanz' = "red",'Trennfähigkeit' = "blue", 'Eingestellte Basisrate' = "green")) #+
         #  annotate("text", x = data_point$prevalence , y = .5 , color = "red", label = "Gewählte Basisrate", angle = 90)
         #    geom_text(x=as.numeric(data_point$prevalence %>% max()), label="the weak cars", y=.5, colour="blue", angle=90)
         
-        p <- ggplotly(p, tooltip = c("Relevanz","Trennfähigkeit"))
+        p <- ggplotly(ptooltip = c("Basisrate", "Relevanz", "Trennfähigkeit"))
+#       p <- ggplotly(p, tooltip = c("Erfasste_Infizierte", "Berechnete_Infizierte", "Tag", "Erfasste_Todesfaelle", "Berechnete_Todesfaelle"))
+
         p <- p %>% layout(legend = list(x = 0.45, y = 0.21, font = list(size = 12)))
         p
     })
